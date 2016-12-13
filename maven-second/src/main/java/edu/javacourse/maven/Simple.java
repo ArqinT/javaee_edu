430b616cbd251c11ad91565ecf939ac0c4b08d3f
Артем Турсунов <artem.tur@gmail.com>
2016-12-13 16:13:54 +0300

    Maven-second

---
+++ maven-second/src/main/java/edu/javacourse/maven/Simple.java
@@ -0,0 +1,11 @@
+package edu.javacourse.maven;
+
+/**
+ * Created by artemtursunov on 13.12.16.
+ */
+public class Simple
+{
+    public void method() {
+        System.out.println("Method for Simple");
+    }
+}
---
+++ maven-second/src/test/java/edu/javacourse/maven/SimpleTest.java
@@ -0,0 +1,12 @@
+package edu.javacourse.maven;
+
+/**
+ * Created by artemtursunov on 13.12.16.
+ */
+public class SimpleTest
+{
+    @org.junit.Test
+    public void method() throws Exception {
+        System.out.println("Test is done");
+    }
+}
